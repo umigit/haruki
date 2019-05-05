daae78fe94252fd0314076a056908b9221b2aaf6
umigit <hamu56Kiki>
2019-05-05 14:31:42 +0900

    add header to books page

---
+++ pages/books.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <TheHeader/>
+  </div>
+</template>
+
+<script>
+import TheHeader from '~/components/TheHeader'
+export default {
+  components: {
+    TheHeader,
+  }
+}
+</script>
+
+<style>
+
+</style>
+
